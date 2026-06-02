17cd4745e3fda68f9f87d1d9a4f45262fd8733f8
Marcos Flavio <mfghost69@gmail.com>
2026-06-02 11:03:38 -0300

    example_env

--- lib/seeker/organizations/just_travel/contexts/insurances.ex
+++ lib/seeker/organizations/just_travel/contexts/insurances.ex
@@ -23,6 +23,21 @@ defmodule Seeker.JustTravel.Contexts.Insurances do
         ORDER BY inserted_at DESC
         LIMIT 20;
         """
+      },
+      %{
+        context: @context,
+        key: :gta_catalog_test,
+        name: "2. GTA catalog",
+        sql: """
+        SELECT
+        	id,
+        state,
+        inserted_at
+        FROM oban_jobs
+        WHERE queue = 'gta_catalog_sync'
+        ORDER BY inserted_at DESC
+        LIMIT 10;
+        """
       }
     ]
     |> Enum.map(&Map.update!(&1, :sql, fn s -> String.trim(s) end))
